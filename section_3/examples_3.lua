11a540c47cab4b2aa5ec865502bf36fb126520ea
lalobgc <lalobg.c@gmail.com>
2025-02-12 17:38:48 -0600

    made a single file for examples in chapter 3

--- section_3/examples_3.lua
+++ section_3/examples_3.lua
@@ -1,3 +1,26 @@
+print(13 + 15)
+print(13.0 + 15.0)
+print(13.0 + 25)
+print(-(3 * 6.0))
+print(3.0 / 2.0)
+print(3 / 2)
+
+print("--------------")
+print("Floor division")
+print("--------------")
+print(3 // 2)
+print(3.0 // 2)
+print(6 // 2)
+print(6.0 // 2.0)
+print(-9 // 2)
+print(1.5 // 0.5)
+
+
+print("---------------------------------")
+print("Modulo operation is defined by")
+print("a % b == a - ((a // b) * b)")
+print("---------------------------------")
+
 print(type(3))
 print(type(3.5))
 print(type(3.0))
